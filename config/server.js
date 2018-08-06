c97aad6448010c5413e8bc35e0b550f7753680fc
Gabriel Felipe <Gabriel_fcs@outlook.com>
2018-08-06 17:18:34 -0300

    add sweetalert
    add sweetalert

--- config/server.js
+++ config/server.js
@@ -7,6 +7,7 @@ var consoleCommand = require('../consoleExec')
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
 app.use('/images', express.static('images'));
 
@@ -16,17 +17,29 @@ app.use(bodyParser.json());
 app.post('/index/clicked', (req, res) => { 
     
     var json = req.body;
+    global.strError = '';
+    var b = new consoleCommand();
+
     for(i in json) {
         var array = json[i].split(',');
         array.forEach(function (element, index) {
-            // consoleCommand.execute('echo executing on: '+element);
-            consoleCommand.execute('(echo execute: '+element+') && (pushd '+element+') && compass compile && echo passed!: '+element+')');
+
+
+            b.execCommand('(echo execute: '+element+') && (pushd '+element+') && compass compile && echo passed!: '+element+')', function(e){
+                console.log("afterall: "+e);
+            });
+            // if(last && consoleOut.length > 0) {
+            //     res.send(200, {
+            //         text: strError
+            //     });
+            // }
             // consoleCommand.execute('start cmd.exe /K "((pushd '+element+') && compass clean && compass compile && exit 0 ) || exit 1 "');
             // consoleCommand.execute('pushd '+element+' && compass compile');
 
             //(pushd D:\htdocs\sprint-extremeuv\web-extreme-uv) && ( compass clean && compass compile && exit ) ||  
         });
     }
+    // console.log("afterall: "+b);
 });
 
 module.exports = app;
--- consoleExec.js
+++ consoleExec.js
@@ -1,23 +1,57 @@
 const { exec } = require('child_process');
 const color = require('./colors');
-
-exports.execute = function (strcommand) {
-    var execprocess = exec(strcommand,{maxBuffer: 1024 * 500}, (error, stdout, stderr) => {
-        if (error) {
-            // console.log('codigo de erro: '+error.code);
-            console.error(color.colorize(`${error}`, color.RED));
-            return;
-        } else {
-            console.log(stderr);
-            if(execprocess.code == 1){
-                console.log(execprocess.code);
+global.strError;
+global.strError = '';
+module.exports = function(){
+    this.execCommand = function(strcommand, callback){
+        exec(strcommand,{maxBuffer: 1024 * 500}, (error, stdout, stderr, ) => {
+            if (error) {
+                global.strError += error;
+                // console.log("afterset: "+error);
+                console.error(color.colorize(`${error}`, color.RED));   
+                return;
+            } else {
+                console.log(stderr);
+                // if(execprocess.code == 1) {
+                //     console.log(execprocess.code);
+                // }
             }
-        }
+            
+            console.log(`${stdout}`);
+            if(stderr.length > 0){
+                console.log(`${stderr}`);
+            }
+            // console.log("beforereturn: "+execprocess.stdout);
+            // return `${strError}`;
+            callback(global.strError);
+        });
+    };
+    return this;
+}
 
+// module.exports.execute = function (strcommand, callback) {
+//     var execprocess = exec(strcommand,{maxBuffer: 1024 * 500}, (error, stdout, stderr, ) => {
+//         if (error) {
+//             global.strError += error;
+//             // console.log("afterset: "+error);
+//             console.error(color.colorize(`${error}`, color.RED));   
+//             return;
+//         } else {
+//             console.log(stderr);
+//             // if(execprocess.code == 1) {
+//             //     console.log(execprocess.code);
+//             // }
+//         }
 
-        console.log(`${stdout}`);
-        if(stderr.length > 0){
-            console.log(`${stderr}`);
-        }
-    });
-};
+//         console.log(`${stdout}`);
+//         if(stderr.length > 0){
+//             console.log(`${stderr}`);
+//         }
+//         // console.log("beforereturn: "+execprocess.stdout);
+//         // return `${strError}`;
+//     });
+//     execprocess.on('close', function (){
+//         return global.strError;
+//     });
+//     callback();
+// };
